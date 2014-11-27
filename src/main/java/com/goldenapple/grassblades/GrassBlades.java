f01a8215441ec5088d5d239c39b14fe512169ae3
goldenapple3 <goldenappleminecraft@gmail.com>
2014-11-27 18:15:26 +0200

    Stuff

--- src/main/java/com/goldenapple/grassblades/GrassBlades.java
+++ src/main/java/com/goldenapple/grassblades/GrassBlades.java
@@ -2,7 +2,13 @@ package com.goldenapple.grassblades;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
 @Mod(modid = "grassblades", version = "1.0", name = "Grass Blades")
 public class GrassBlades{
@@ -10,6 +16,19 @@ public class GrassBlades{
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        MinecraftForge.EVENT_BUS.register(new FMLEventHandler());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerStartBreaking(PlayerEvent.BreakSpeed event){ //every time a player starts breaking a block
+        if(event.entityPlayer.getCurrentEquippedItem() != null){ //if the player has an item equipped
+            if(event.entityPlayer.getCurrentEquippedItem().getItem() instanceof ItemSword) { //if the item is a sword
+                World world = event.entityPlayer.worldObj;
+                Block block = world.getBlock(event.x, event.y, event.z);
+                if(block.getBlockHardness(world, event.x, event.y, event.z) == 0.0F){
+                    event.newSpeed = -1.0F;
+                }
+            }
+        }
     }
 }
